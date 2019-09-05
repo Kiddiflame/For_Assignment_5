6319a0d4c557fa0b496564f4f2ccccf4d7e30f61
unknown <kiddiflame@gmail.com>
2019-09-05 09:58:14 +0000

    Algorithm written for sequence

---
+++ Documents/Forritun/sequence.py
@@ -0,0 +1,7 @@
+#1. Tekur inn tölu sem ákveður hversu margar tölur eru prentaðar
+#2. Skilgreinir tölu 1 og tölu 2
+#3. for-loopa er byrjuð
+#4. Ef tala 1 er 0, er hún gefin tölu jöfn og i, ef tala 2 er 0, er hún gefin gildi af i+1 
+#5. Ef tala 1 og 2 hafa gildi, tala 3 er jöfn og tala 1 og 2 summuð
+#6. Tala 3 er gerð að tölu 2 og 2 að 1
+#7 Endurtaka skref 5 og 6
